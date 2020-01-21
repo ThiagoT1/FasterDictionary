06d2da72e0c4d1cedfadc73d6dea69565620acc0
ThiagoT1 <thiagocodigo@gmail.com>
2020-01-20 23:41:30 -0300

    WIP

---
+++ src/Json.NET.Fallback.Converter/JsonNETDefault.cs
@@ -0,0 +1,21 @@
+﻿using Json.NET.Fallback.Converter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Json.NET.Fallback
+{
+    public static class JsonNETDefault
+    {
+        public static JsonSerializerOptions Options { get; set; }
+        static JsonNETDefault()
+        {
+            Options = new JsonSerializerOptions()
+            {
+                IgnoreNullValues = true
+            };
+            Options.Converters.Add(new JsonNETConverterFactory());
+        }
+    }
+}
